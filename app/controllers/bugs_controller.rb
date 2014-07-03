8684e1f07a6e28c259b50ecf5915aeaf277aa96f
Alexandra Vtyurina <sasha.vtyurina@gmail.com>
2014-07-03 17:44:29 +0400

    creating-new-bugs-page

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,8 +1,24 @@
 class BugsController < ApplicationController
   def new
+  	@bug = Bug.new
   end
 
   def show
   	@bug = Bug.find(params[:id])
   end
+
+  def create
+  	@bug = Bug.new(bug_params)
+  	if @bug.save
+  		redirect_to @bug
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+    def bug_params
+    	params.require(:bug).permit(:description, :application_name)
+    end
+
 end
